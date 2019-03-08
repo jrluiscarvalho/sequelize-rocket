7f3605aac424cd068923587e16fc62ee2312ad70
luisCarvalhoJr <carvalholuis@bitbucket.com>
2019-03-08 09:52:12 -0300

    first commit

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'mysql',
+  host: '127.0.0.1',
+  username: 'root',
+  password: '123',
+  database: 'gonodemodulo2',
+  operatorAliases: false,
+  define: {
+    timestamps: true,
+    underscore: true,
+    underscoreAll: true
+  }
+}
